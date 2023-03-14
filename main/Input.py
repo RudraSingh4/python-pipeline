64b89afb661477d5a79a44f82d6e2ddb742bb534
Rudra <rishurudrasingh@gmail.com>
2023-03-14 17:20:42 +0530

    Add input

--- main/Input.py
+++ main/Input.py
@@ -1,16 +1,8 @@
-# Python program to add two numbers with user input
-
-def add_num(a,b):   #user-defined function
-   sum = a + b   #adding numbers
-   return sum   #return value
-
-# take inputs
-num1 = float(input('Enter first number: '))
-num2 = float(input('Enter second number: '))
-
-# calling function
-sum = add_num(num1, num2)
-
-# print sum of numbers
-print('The sum of numbers {0} and {1} is {2}'
-                      .format(num1, num2, sum))
+import sys
+print("Inputs:")
+one = sys.stdin.readline()
+two = sys.stdin.readline()
+print("Output:")
+three = int(one) + int(two)
+four = str(three)
+sys.stdout.write(four)
