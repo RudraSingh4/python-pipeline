4f94f4022d625db6a55ad1fe341216089dcff898
Rudra <rishurudrasingh@gmail.com>
2023-03-14 18:00:54 +0530

    Modify Input

--- main/Input.py
+++ main/Input.py
@@ -1,13 +1,22 @@
+# import sys
+
+
+# def mul(a, b):
+#     x = a*b
+#     return x
+
+# a = int(sys.argv[1])
+# b = int(sys.argv[2])
+# # c = sys.argv[3]
+
+# print(f'Multiply to {a} and {b} = {mul(a, b)}')
+# # print("!! Hello", c, "!!")
+
 import sys
-
-
-def mul(a, b):
-    x = a*b
-    return x
-
-a = int(sys.argv[1])
-b = int(sys.argv[2])
-# c = sys.argv[3]
-
-print(f'Multiply to {a} and {b} = {mul(a, b)}')
-# print("!! Hello", c, "!!")
+print("Inputs:")
+one = sys.stdin.readline()
+two = sys.stdin.readline()
+print("Output:")
+three = int(one) + int(two)
+four = str(three)
+sys.stdout.write(four)
