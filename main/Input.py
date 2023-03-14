9b547369ed20a856ebc9272883475e8c1095b637
Rudra <rishurudrasingh@gmail.com>
2023-03-14 18:05:01 +0530

    Modify  input

--- main/Input.py
+++ main/Input.py
@@ -12,11 +12,13 @@
 # print(f'Multiply to {a} and {b} = {mul(a, b)}')
 # # print("!! Hello", c, "!!")
 
-import sys
-print("Inputs:")
-one = sys.stdin.readline()
-two = sys.stdin.readline()
-print("Output:")
-three = int(one) + int(two)
-four = str(three)
-sys.stdout.write(four)
+# This program adds two numbers
+
+num1 = 1.5
+num2 = 6.3
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
