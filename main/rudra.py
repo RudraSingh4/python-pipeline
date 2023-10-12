4c4f2390476a01a4df6c38911e9960581a9abe12
Rudra Singh <52309515+RudraSingh4@users.noreply.github.com>
2023-10-12 16:58:02 +0530

    update code

--- main/rudra.py
+++ main/rudra.py
@@ -1,1 +1,9 @@
-print("Hello ")
+winning_number = 27
+guessed_number = int(input("Enter the guess number: "))
+if winning_number==guessed_number:
+    print("YOU WIN !!!!")
+else:
+    if guessed_number<winning_number:
+        print("Too Low")
+    else:
+        print("Too High")
